18f9b4e7c15c51303cc7d3036e8e48bc32db276d
yugkumawat-123 <yugkumawat03@gmail.com>
2024-07-07 15:34:57 +0530

    installed react-query . replaced usShowById with UseQuery hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,26 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Home from './pages/Home';
 import Starred from './pages/Starred';
 import Show from './pages/Show';
 import MainLayout from './components/MainLayout';
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<MainLayout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/Starred" element={<Starred />} />
-        </Route>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<MainLayout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/Starred" element={<Starred />} />
+          </Route>
 
-        <Route path="/show/:showId" element={<Show />} />
-        <Route path="*" element={<div>Not Found</div>} />
+          <Route path="/show/:showId" element={<Show />} />
+          <Route path="*" element={<div>Not Found</div>} />
 
-        {/* <Route path="/" element={<App />}>
+          {/* <Route path="/" element={<App />}>
       <Route index element={<Home />} />
       <Route path="teams" element={<Teams />}>
         <Route path=":teamId" element={<Team />} />
@@ -29,8 +33,9 @@ function App() {
       <Route path="/tos" element={<Tos />} />
     </Route>
     <Route path="contact-us" element={<Contact />} /> */}
-      </Routes>
-    </BrowserRouter>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
