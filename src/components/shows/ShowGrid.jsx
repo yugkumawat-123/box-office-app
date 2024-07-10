e5c3f36e30f8af7357685313506cbbfa8d252774
yugkumawat-123 <yugkumawat03@gmail.com>
2024-07-10 20:43:02 +0530

    added inital logic to star with showReducer . created usePersistedReducer to sync state with localstorage

--- src/components/shows/ShowGrid.jsx
+++ src/components/shows/ShowGrid.jsx
@@ -1,6 +1,49 @@
+import { useEffect, useReducer } from 'react';
 import ShowCard from './ShowCard';
 
+const usePersistedReducer = (reducer, initialState, localStorageKey) => {
+  const [state, dispatch] = useReducer(reducer, initialState, initial => {
+    const persistedValue = localStorage.getItem(localStorageKey);
+    return persistedValue ? JSON.parse(persistedValue) : initial;
+  });
+
+  useEffect(() => {
+    localStorage.setItem(localStorageKey, JSON.stringify(state));
+  }, [state, localStorageKey]);
+
+  return [state, dispatch];
+};
+
+const starredShowsReducer = (currentStarred, action) => {
+  switch (action.type) {
+    case 'STAR':
+      return currentStarred.concat(action.showId);
+
+    case 'UNSTAR':
+      return currentStarred.filter(showId => showId !== action.showId);
+
+    default:
+      return currentStarred;
+  }
+};
+
 const ShowGrid = ({ shows }) => {
+  const [starredShows, dispatchStarred] = usePersistedReducer(
+    starredShowsReducer,
+    [],
+    'starredShows'
+  );
+
+  const onStarMeClick = showId => {
+    const isStarred = starredShows.includes(showId);
+
+    if (isStarred) {
+      dispatchStarred({ type: 'UNSTAR', showId });
+    } else {
+      dispatchStarred({ type: 'STAR', showId });
+    }
+  };
+
   return (
     <div>
       {shows.map(data => (
@@ -12,9 +55,11 @@ const ShowGrid = ({ shows }) => {
             data.show.image ? data.show.image.medium : '/not-found-image.png'
           }
           summary={data.show.summary}
+          onStarMeClick={onStarMeClick}
         />
       ))}
     </div>
   );
 };
+
 export default ShowGrid;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -45,6 +45,7 @@ const Home = () => {
   return (
     <div>
       <SearchForm onSearch={onSearch} />
+
       <div>{renderApiData()}</div>
     </div>
   );
